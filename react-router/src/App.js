39cc12795b07025a2563769a3c692c80d3514db1
twedty <jamestwedt@gmail.com>
2024-05-29 22:04:30 -0400

    installed 2 different libraries to make sure the code worked

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 
 import './App.css';
 
@@ -7,29 +8,39 @@ import Packages from './components/Packages'
 
 
 function App() {
-
+  
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
+
 
   return (
     <div className="App">
-      <header>
-        <h1 className="title">Welcome to Monty's Mineral SPA</h1>
+      <Router>
+        <header>
+          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
 
-        <div className="navBar">
-          <ul>
-            <li>
-              <a href={<Home />}>Home</a>
-            </li>
-            <li>
-              <a href={<About />}>About Us</a>
-            </li>
-            <li>
-              <a href={<Packages packages={packages}/>}>Our Packages</a>
-            </li>
-          </ul>
+          <div className="navBar">
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/packages">Our Packages</Link>
+              </li>
+            </ul>
+          </div>
+        </header>
+
+        <div className="display">
+          <Route path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
         </div>
 
-      </header>
+      </Router>
+
     </div>
   );
 }
--- react-router/src/index.js
+++ react-router/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,5 +10,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 
